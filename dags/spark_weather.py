5cedc6aa3d80b51b928a124255a58b8a04bf64bd
MuhammadTomyNM <tomymaulidu@gmail.com>
2025-09-02 11:06:26 +0700

    update

---
+++ dags/spark_weather.py
@@ -0,0 +1,37 @@
+from pyspark.sql import SparkSession
+import requests
+from datetime import datetime
+
+spark = SparkSession.builder.appName("WeatherToKudu").getOrCreate()
+
+API_KEY = "43815e6e6e786744b176b29a5819423f"
+CITY = "Jakarta"
+
+url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+data = requests.get(url).json()
+
+if "main" not in data:
+    raise ValueError(f"API error: {data}")
+
+record = [
+    (
+        CITY,
+        data["main"]["temp"],
+        data["main"]["humidity"],
+        data["main"].get("pressure", 0),
+        data["weather"][0]["description"] if "weather" in data else "unknown",
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    )
+]
+
+columns = ["city", "temp", "humidity", "pressure", "weather_desc", "ts"]
+
+df = spark.createDataFrame(record, columns)
+
+# Tulis ke Kudu
+df.write \
+  .format("kudu") \
+  .option("kudu.master", "edge.ds-inovasi.com:21050") \
+  .option("kudu.table", "mhmadtomy.weather_data") \
+  .mode("append") \
+  .save()
